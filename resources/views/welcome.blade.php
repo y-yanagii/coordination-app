5ca006a77bf12afc6e87882b649484ea0b7b48b4
y-yanagii <yyanagisawa555@gmail.com>
2020-06-21 19:01:33 +0900

    add vuejs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
         <!-- Styles -->
         <style>
@@ -95,6 +97,12 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
+
+            <div id="app">
+                <example-component></example-component>
+            </div>
         </div>
+
+        <script src=" {{ mix('js/app.js') }} "></script>
     </body>
 </html>
